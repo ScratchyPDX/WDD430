74c4508a99af99abe622d8657313a700bcfc4899
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-10-02 16:52:14 -0700

    Finished adding csm components

---
+++ cms/src/app/messages/message-edit/message-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-edit',
+  standalone: false,
+  templateUrl: './message-edit.html',
+  styleUrl: './message-edit.css'
+})
+export class MessageEdit {
+
+}
