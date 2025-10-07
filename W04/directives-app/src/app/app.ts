ac6cb4bf2973e2d4e8f3a0da1998ef2e16880889
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-10-06 18:33:32 -0700

    Initial check-in of W04 learning project

---
+++ W04/directives-app/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
+  standalone: false
+})
+export class App {
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
+}
