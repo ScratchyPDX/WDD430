45a48525c617fb07a64135d766531edadc2baa6a
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-10-07 17:30:20 -0700

    Update cms document list component

--- cms/src/app/documents/document-list/document-list.ts
+++ cms/src/app/documents/document-list/document-list.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.css'
 })
 export class DocumentList {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  @Input() documents: Document[] = [
+    new Document("1", "Document 1", "Document 1 description", "url", []),
+    new Document("2", "Document 2", "Document 2 description", "url", []),
+    new Document("3", "Document 3", "Document 3 description", "url", []),
+    new Document("4", "Document 4", "Document 4 description", "url", []),
+  ]
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit()
+  }
 
 }
