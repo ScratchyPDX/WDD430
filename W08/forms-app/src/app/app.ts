1d3064b56453036310f1e15345877346560d0ba9
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-10-29 18:41:19 -0700

    W08 TD driven form complete before assignment

--- W08/forms-app/src/app/app.ts
+++ W08/forms-app/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,53 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion='pet';
+  answer='';
+  genders=['male','female'];
+  user: {
+    username: string,
+    email: string,
+    secretQuestion: string,
+    questionAnswer: string,
+    gender: string
+  };
+  submitted = false;
 
   suggestUserName() {
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: 'Superuser',
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.user = {
+      username: this.signupForm.value.userData.username,
+      email: this.signupForm.value.userData.email,
+      secretQuestion: this.signupForm.value.secret,
+      questionAnswer: this.signupForm.value.questionAnswer,
+      gender: this.signupForm.value.gender
+    };
+    this.submitted = true;
+    this.signupForm.reset();
   }
 }
