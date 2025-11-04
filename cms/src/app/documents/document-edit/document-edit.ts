2b8196e32f79f226e130450a459765b485de4d33
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-11-03 18:47:20 -0800

    W08 cms implement document edit

--- cms/src/app/documents/document-edit/document-edit.ts
+++ cms/src/app/documents/document-edit/document-edit.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-document-edit',
@@ -8,7 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentEdit implements OnInit {
 
+  @ViewChild('f') signupForm: NgForm;
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+
+  constructor(private documentService: DocumentService, private router: Router, private route: ActivatedRoute) { }
+
   ngOnInit() {
-    console.log('DocumentEdit initialized');
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      console.log("ngOnInit called with id:", id);
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = { ...this.originalDocument };
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log("onSubmit called");
+    const newDocument = new Document('', form.value.name, form.value.description, form.value.url, []);
+    console.log(newDocument);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['/documents']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
   }
 }
