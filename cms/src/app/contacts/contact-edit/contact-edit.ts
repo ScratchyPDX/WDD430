01fa7568cc09bcc4b33c117ff4e31d8f6f2a31b2
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-11-04 17:53:24 -0800

    W08 cms add group contact feature

--- cms/src/app/contacts/contact-edit/contact-edit.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.ts
@@ -12,13 +12,16 @@ import { ContactService } from '../contact.service';
 export class ContactEdit implements OnInit{
   contact: Contact = {id: '', name: '', email: '', phone: '', imageUrl: '', group: []};
   originalContact: Contact;
-  groupContacts: any[] = [];
+  groupContacts: Contact[] = [];
   editMode: boolean = false;
   id: string;
+  availableContacts: Contact[] = [];
+  selectedContactId: string = '';
 
   constructor(private contactService: ContactService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.availableContacts = this.contactService.getContacts();
     this.route.params.subscribe(params => {
       this.id = params['id'];
       this.editMode = !!this.id;
@@ -31,22 +34,51 @@ export class ContactEdit implements OnInit{
       if (this.contact.group) {
         this.groupContacts = [...this.contact.group];
       }
+      this.updateAvailableContacts();
     });
+  }
 
+  updateAvailableContacts() {
+    const allContacts = this.contactService.getContacts();
+    const groupContactIds = this.groupContacts.map(c => c.id);
+
+    this.availableContacts = allContacts.filter((contact) => {
+      !groupContactIds.includes(contact.id) && contact.id !== this.contact.id
+    });
   }
 
   onSubmit() {
-   const newContact = new Contact('', this.contact.name, this.contact.email, this.contact.phone, this.contact.imageUrl, this.groupContacts);
-   if (this.editMode) {
-     this.contactService.updateContact(this.originalContact, newContact);
-   } else {
-     this.contactService.addContact(newContact);
-   }
+    const newContact = new Contact('', this.contact.name, this.contact.email, this.contact.phone, this.contact.imageUrl, this.groupContacts);
+    if (this.editMode) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
     this.onCancel();
   }
 
   onCancel() {
     this.router.navigate(['/contacts']);
+  }
+
+  onAddContactToGroup() {
+    if (!this.selectedContactId) {
+      return;
+    }
+
+    const contactToAdd = this.contactService.getContact(this.selectedContactId);
+    if (contactToAdd && !this.groupContacts.find(c => c.id === contactToAdd.id)) {
+      this.groupContacts.push(contactToAdd);
+      this.selectedContactId = ''; // Reset selection
+      this.updateAvailableContacts(); // Update available list
+    }
+  }
+
+  onRemoveContactFromGroup(index: number) {
+    if (index >= 0 && index < this.groupContacts.length) {
+      this.groupContacts.splice(index, 1);
+      this.updateAvailableContacts(); // Update available list
+    }
   }
 
 }
