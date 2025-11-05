0d182bee9bdd2668d8f6f3eb60172485100eb431
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-11-04 17:19:13 -0800

    W08 cms add ability to add new contact

--- cms/src/app/contacts/contact-edit/contact-edit.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Contact } from '../contact/contact.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-edit',
@@ -8,18 +10,43 @@ import { Contact } from '../contact/contact.model';
   styleUrl: './contact-edit.css'
 })
 export class ContactEdit implements OnInit{
-  @ViewChild('contactForm') contactForm: any;
   contact: Contact = {id: '', name: '', email: '', phone: '', imageUrl: '', group: []};
+  originalContact: Contact;
   groupContacts: any[] = [];
+  editMode: boolean = false;
+  id: string;
 
-  constructor() { }
+  constructor(private contactService: ContactService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    // Initialize the component
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      this.editMode = !!this.id;
+      this.originalContact = this.contactService.getContact(this.id);
+      if (!this.originalContact) {
+        return;
+      }
+      this.editMode = true;
+      this.contact = { ...this.originalContact };
+      if (this.contact.group) {
+        this.groupContacts = [...this.contact.group];
+      }
+    });
+
   }
 
   onSubmit() {
-    // Handle form submission
+   const newContact = new Contact('', this.contact.name, this.contact.email, this.contact.phone, this.contact.imageUrl, this.groupContacts);
+   if (this.editMode) {
+     this.contactService.updateContact(this.originalContact, newContact);
+   } else {
+     this.contactService.addContact(newContact);
+   }
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
   }
 
 }
