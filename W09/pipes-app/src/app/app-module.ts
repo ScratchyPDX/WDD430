4131c137fba9f65fb3727a0ab232dc1763b9dee9
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2025-11-08 09:57:55 -0800

    W09 Initial pipes-app check-in part 2

--- W09/pipes-app/src/app/app-module.ts
+++ W09/pipes-app/src/app/app-module.ts
@@ -2,13 +2,15 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     App
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
